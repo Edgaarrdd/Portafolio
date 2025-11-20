a66b6ee2c3e653a3d43b407c1e4ccc06b1a72d0e
Edgard <edgardmachucacode@gmail.com>
2025-11-20 01:39:58 -0300

    Primer commit: subida inicial del proyecto

---
+++ tailwind-config.js
@@ -0,0 +1,23 @@
+tailwind.config = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#007AFF',
+        'background-light': '#f6f7f8',
+        'background-dark': '#0D1117',
+      },
+      fontFamily: {
+        display: ['Space Grotesk', 'sans-serif'],
+      },
+      borderRadius: {
+        DEFAULT: '0.5rem',
+        lg: '1rem',
+        xl: '1.5rem',
+        full: '9999px',
+      },
+    },
+  },
+};
+
+
